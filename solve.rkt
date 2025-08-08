efecaad9daabed73217ee09f4c2c3f71337dec01
Xiangyu Gao <xiangyug@cs.washignton.edu>
2025-08-07 21:17:45 -0600

    Update solve tutorial

--- solve.rkt
+++ solve.rkt
@@ -1,20 +1,17 @@
 #lang rosette/safe
 
+;;; (require racket/base)  ;; this loc would cause wrong solve solution
+
 (define-symbolic x (bitvector 8))
-(define-symbolic y (bitvector 8))
 
 (define (f x)
   (cond
     [(bvult x (bv 10 8)) (bvadd x (bv 5 8))]
     [else               (bvsub x (bv 5 8))]))
 
-(define result (f (bv 7 8)))
-(displayln result)                      ; prints bitvector like #x0c (12)
-
 (define sol
   (solve
    (assert (equal? (f x) (bv 12 8))))
-
   )
 
 (displayln sol)
