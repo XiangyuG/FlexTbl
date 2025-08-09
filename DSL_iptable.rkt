3bb6801202b21824427118af5245ed9594af44d0
xiang95 <xiang95@node0.parser.meshbench-pg0.utah.cloudlab.us>
2025-08-09 11:14:27 -0600

    First file to support NAT and filtering for iptable in Rosette

---
+++ DSL_iptable.rkt
@@ -0,0 +1,187 @@
+#lang rosette
+
+(require rosette/lib/angelic    ; provides choose*
+         rosette/lib/destruct)  ; provides destruct
+
+(require rosette/lib/synthax)
+(require racket/match)
+(require racket/string)   ; 导入 string->split 和相关字符串函数
+(require (only-in racket/base string->number))
+(require (only-in racket/base error))
+
+(define int32? (bitvector 32))
+(define (int32 i) (bv i int32?))
+
+
+(struct Rule (match action rewrite) #:transparent)
+
+(struct Match (src dst proto sport dport) #:transparent)
+(struct Rewrite (new-src new-dst new-sport new-dport) #:transparent)
+
+(struct packet (src dst proto sport dport) #:transparent)
+
+
+(define (match? pkt m)
+  (and
+   (or (equal? (Match-src m) #f)
+       (equal? (packet-src pkt) (Match-src m)))
+   (or (equal? (Match-dst m) #f)
+       (equal? (packet-dst pkt) (Match-dst m)))
+   (or (equal? (Match-proto m) #f)
+       (equal? (packet-proto pkt) (Match-proto m)))
+   (or (equal? (Match-sport m) #f)
+       (equal? (packet-sport pkt) (Match-sport m)))
+   (or (equal? (Match-dport m) #f)
+       (equal? (packet-dport pkt) (Match-dport m)))))
+
+;; generate rules
+(define (eval-packet pkt rules)
+  (define (apply-rewrite pkt rw)
+    (packet
+     (or (Rewrite-new-src rw) (packet-src pkt))
+     (or (Rewrite-new-dst rw) (packet-dst pkt))
+     (packet-proto pkt)
+     (or (Rewrite-new-sport rw) (packet-sport pkt))
+     (or (Rewrite-new-dport rw) (packet-dport pkt))))
+
+  (define (step pkt rules)
+    (cond
+      [(null? rules) (cons (int32 1) pkt)] ; default: drop (1), no change
+      [else
+       (define rule (car rules))
+       (if (match? pkt (Rule-match rule))
+           (let* ([newpkt (apply-rewrite pkt (Rule-rewrite rule))]
+                  [action (Rule-action rule)])
+             (cons action newpkt)) ; return (action . newpkt)
+           (step pkt (cdr rules)))]))
+  (step pkt rules))
+
+
+; accept is 0, drop is 1
+; udp is 0, tcp is 1
+; (src dst proto sport dport)
+
+(define (ip->int32 ip-str)
+  (define parts (map string->number (string-split ip-str ".")))
+  (define ip-int
+    (+ (* (list-ref parts 0) (expt 256 3))
+       (* (list-ref parts 1) (expt 256 2))
+       (* (list-ref parts 2) (expt 256 1))
+       (* (list-ref parts 3) (expt 256 0))))
+  (bv ip-int 32))  ; convert to 32-bit bitvector
+
+(define rule1
+  (Rule
+   (Match #f (ip->int32 "10.0.0.100") (int32 1) #f (int32 80)) ; match dst ip and dport
+   (int32 0)
+   (Rewrite #f (ip->int32 "192.168.0.2") #f #f))) ; DNAT
+
+(define rule2
+  (Rule
+   (Match (ip->int32 "10.0.0.1") #f (int32 1) #f #f) ; match src ip
+   (int32 1)
+   (Rewrite #f #f #f #f))) ; no NAT
+
+(define rules (list rule1 rule2))
+
+(define pkt (packet (ip->int32 "10.0.0.0") (ip->int32 "10.0.0.101") (int32 1) (int32 12345) (int32 80)))
+
+(car (eval-packet pkt rules)) ; → 'accept
+
+
+(define test-pkt
+  (packet (ip->int32 "10.0.0.1") (ip->int32 "1.2.3.4") (int32 1) (int32 1234) (int32 80)))
+
+;; define symbolic variables
+(define-symbolic src_sym int32?)
+(define-symbolic dst_sym int32?)
+(define-symbolic proto_sym int32?)
+(define-symbolic sport_sym int32?)
+(define-symbolic dport_sym int32?)
+
+;; construct symbolic packet
+(define sym-pkt (packet src_sym dst_sym proto_sym sport_sym dport_sym))
+
+;; evaluate with existing rules
+(define result (eval-packet sym-pkt rules))
+result
+(define action (car result))
+
+
+;;; (define sol
+;;;   (solve
+;;;   (assert (equal? action (int32 1))))
+;;; )
+
+;;; (displayln sol)
+
+;;; TODO realize NAT
+(define-grammar (Impl_grammar src_sym dst_sym proto_sym sport_sym dport_sym)
+  [expr
+   (choose
+    (if (cond-expr) (expr) (expr))
+    (cons (?? int32?)
+          (packet (src_fin)  
+                  (dst_fin)  
+                  (proto_fin)  
+                  (sport_fin)  
+                  (dport_fin))) ; return type
+    )]
+  [src_fin
+   (choose
+    src_sym
+    (?? int32?))]
+  [dst_fin
+   (choose
+    dst_sym
+    (?? int32?))]
+  [proto_fin
+   (choose
+    proto_sym
+    (?? int32?))]
+  [sport_fin
+   (choose
+    sport_sym
+    (?? int32?))]
+  [dport_fin
+   (choose
+    dport_sym
+    (?? int32?))]
+  [cond-expr
+   (choose
+    #t
+    (equal? (vexpr) (vexpr))
+    (and (cond-expr) (cond-expr) (cond-expr) (cond-expr) (cond-expr))
+    )]
+
+  [vexpr
+   (choose
+    src_sym
+    dst_sym
+    proto_sym
+    sport_sym
+    dport_sym
+    (?? int32?))]     
+  )
+  
+
+(define (impl-fast srcip dstip proto sport dport)
+  (Impl_grammar srcip dstip proto sport dport #:depth 3))
+
+
+(define sol
+    (synthesize
+     #:forall    (list src_sym dst_sym proto_sym sport_sym dport_sym)
+     #:guarantee (begin
+      (assume (or (equal? proto_sym (int32 0)) (equal? proto_sym (int32 1))))
+      (assert (equal? (impl-fast src_sym dst_sym proto_sym sport_sym dport_sym) 
+                     (eval-packet (packet src_sym dst_sym proto_sym sport_sym dport_sym) rules))))
+    )
+)
+
+(define sol-str
+  (with-output-to-string
+    (lambda () (print-forms sol))))
+
+(displayln "Final output:")
+(displayln sol-str)
