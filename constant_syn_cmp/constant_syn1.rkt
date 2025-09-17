35b34bd915928c8e9f24f03393a413804089062c
Xiangyu Gao <xiangyug@cs.washington.edu>
2025-09-17 07:56:06 -0600

    Update constant synthesis 1

--- constant_syn_cmp/constant_syn1.rkt
+++ constant_syn_cmp/constant_syn1.rkt
@@ -8,7 +8,7 @@
 (define-symbolic srcPort (bitvector 16))
 (define-symbolic dstPort (bitvector 16))
 
-(define (spec0 proto srcIP dstIP srcPort dstPort)
+(define (spec1 proto srcIP dstIP srcPort dstPort)
   (if (and (bveq proto (bv 0 8)) (bveq srcIP (bv 1 32)) (bveq dstIP (bv 0 32)) (bveq srcPort (bv 0 16)) (bveq dstPort (bv 0 16))) (bv 1 8) (bv 0 8)))
 
 (define-grammar (const8)
@@ -21,11 +21,11 @@
   [cst (choose (bv 0 16) (bv 1 16) (bv 42 16) (bv 80 16) (bv 433 16) (bv 21 16) (bv 135 16) (bv 445 16))])
 
 ;; impl:
-(define (impl0 proto srcIP dstIP srcPort dstPort)
+(define (impl1 proto srcIP dstIP srcPort dstPort)
   (if (and (bveq proto (const8)) (bveq srcIP (const32)) (bveq dstIP (const32)) (bveq srcPort (const16)) (bveq dstPort (const16))) (bv 1 8) (bv 0 8))   ;;; We MUST use bveq for synthesis
 )
 
-(define (impl0vanila proto srcIP dstIP srcPort dstPort)
+(define (impl1vanila proto srcIP dstIP srcPort dstPort)
   (if (and (bveq proto (?? (bitvector 8))) (bveq srcIP (?? (bitvector 32))) (bveq dstIP (?? (bitvector 32))) (bveq srcPort (?? (bitvector 16))) (bveq dstPort (?? (bitvector 16)))) (bv 1 8) (bv 0 8))   ;;; We MUST use bveq for synthesis
 )
 
@@ -34,7 +34,7 @@
    #:forall (list proto srcIP dstIP srcPort dstPort)
    #:guarantee
    (begin
-    (assert (bveq (spec0 proto srcIP dstIP srcPort dstPort) (impl0vanila proto srcIP dstIP srcPort dstPort)))
+    (assert (bveq (spec1 proto srcIP dstIP srcPort dstPort) (impl1 proto srcIP dstIP srcPort dstPort)))
    )
    ))
 
