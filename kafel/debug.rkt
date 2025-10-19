59638e4fa11f8f4ea48345796e7e52312c29bdab
xiang95 <xiang95@node0.simplebpf1.meshbench-pg0.clemson.cloudlab.us>
2025-10-19 15:56:24 -0400

    Program to output performance-aware code

--- kafel/debug.rkt
+++ kafel/debug.rkt
@@ -10,37 +10,53 @@
 (define-symbolic z (bitvector 8))
 
 ;; --- define symbolic costs ---
-(define-symbolic cost1 cost2 cost3 integer?)
-(assert (and (>= cost1 0) (= cost2 0) (= cost3 0)))
+(define-symbolic cost0 cost0L cost0R integer?)
+(assert (and (>= cost0 0) (>= cost0L 0) (>= cost0R 0)))
 
 ;; --- define implementation (no grammar) ---
 (define (impl x y z)
   (let* (
-        ;;; [a (choose (bv 0 1) (bv 1 1))]            ; choose #1
-        ;;; ;;;  [_ (if (bveq a (bv 0 1)) (assert (= cost1 0)) (assert (= cost1 1)))]
-         
-        ;;;  [b (choose (bvand x (bv 0 4))
-        ;;;             (bvor x (bv 1 4)))]            ; choose #2
-        ;;;  [_ (if (equal? b (bvand x (bv 0 4)))
-        ;;;         (assert (= cost2 0))
-        ;;;         (assert (= cost2 0)))]
-         
-        ;;;  [c (choose (bveq y (bv 0 1))
-        ;;;             (bveq z (bv 0 8)))]            ; choose #3
-        ;;;  [_ (if (equal? c (bveq y (bv 0 1)))
-        ;;;         (assert (= cost3 0))
-        ;;;         (assert (= cost3 0)))]
          [cond-expr0 ((choose bveq bvslt) 
          (choose x y z ((choose bvand bvor) (choose x y z (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8)) (choose x y z (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8))))
          (choose (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8)))]
-         [expr0L (choose (bv 0 1)  ;;; ALLOW
+         [cond-expr0L ((choose bveq bvslt) 
+         (choose x y z ((choose bvand bvor) (choose x y z (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8)) (choose x y z (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8))))
+         (choose (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8)))]
+         [cond-expr0R ((choose bveq bvslt) 
+         (choose x y z ((choose bvand bvor) (choose x y z (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8)) (choose x y z (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8))))
+         (choose (bv 0 4) (bv 1 4) (bv 2 4) (bv 0 1) (bv 1 1) (bv 0 8)))]
+         
+         [expr0LL (choose (bv 0 1)  ;;; ALLOW
                           (bv 1 1)  ;;; KILL
                           )
          ]
-         [expr0R (choose (bv 0 1)  ;;; ALLOW
+         [expr0LR (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          ) ]
+         [expr0RL (choose (bv 0 1)  ;;; ALLOW
                           (bv 1 1)  ;;; KILL
                           )
          ]
+         [expr0RR (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          ) ]
+         [choiceexpr0L (choose 0 1 2)]
+         [expr0L (cond
+            [(= choiceexpr0L 0) (bv 0 1)]  ;;; ALLOW
+            [(= choiceexpr0L 1) (bv 1 1)]  ;;; KILL
+            [else (if cond-expr0L expr0LL expr0LR)]
+            )
+         ]
+         [_ (if (or (= choiceexpr0L 0) (= choiceexpr0L 1)) (assert (= cost0L 0)) (assert (= cost0L 1)))]
+         [choiceexpr0R (choose 0 1 2)]
+         [expr0R (cond
+            [(= choiceexpr0R 0) (bv 0 1)]  ;;; ALLOW
+            [(= choiceexpr0R 1) (bv 1 1)]  ;;; KILL
+            [else (if cond-expr0R expr0RL expr0RR)]
+            )
+         ]
+         [_ (if (or (= choiceexpr0R 0) (= choiceexpr0R 1)) (assert (= cost0R 0)) (assert (= cost0R 1)))]
+         
          [choiceexpr0 (choose 0 1 2)]
          [expr0 (cond
             [(= choiceexpr0 0) (bv 0 1)]  ;;; ALLOW
@@ -48,16 +64,21 @@
             [else (if cond-expr0 expr0L expr0R)]
             )
          ]
-         [_ (if (or (= choiceexpr0 0) (= choiceexpr0 1)) (assert (= cost1 0)) (assert (= cost1 1)))]
+         [_ (if (or (= choiceexpr0 0) (= choiceexpr0 1)) (assert (= cost0 0)) (assert (= cost0 1)))]
          )
     ;; final output
     expr0))
 
 ;; total cost = sum of all symbolic costs
-(define total-cost (+ cost1 cost2 cost3))
+(define total-cost (+ cost0 cost0L cost0R))
 
 ;; --- define spec ---
-(define (spec x y z) (bv 0 1))
+(define (spec x y z)
+  (cond
+    ;;; [(and (bveq x (bv 0 4)) (bveq y (bv 0 1)) (bveq z (bv 0 8))) (bv 0 1)] ;; ALLOW
+    ;;; [(bveq x (bv 0 4)) (bv 0 1)] ;; ALLOW 1 condition
+    [(and (bveq x (bv 0 4)) (bveq y (bv 0 1))) (bv 0 1)] ;; ALLOW
+    [else               (bv 1 1)]))   ;; KILL
 
 ;; --- optimize to minimize total cost ---
 (define sol
@@ -65,7 +86,7 @@
      #:forall    (list x y z)
      #:guarantee (begin
     ;;;   (assume (or (equal? proto_sym (int32 0)) (equal? proto_sym (int32 1))))
-      (assert (and (bveq (impl x y z) (spec x y z)) (= total-cost 0)))
+      (assert (and (bveq (impl x y z) (spec x y z)) (= total-cost 3)))
     ))
 )
 
