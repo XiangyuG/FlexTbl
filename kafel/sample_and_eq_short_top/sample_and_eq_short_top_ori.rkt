d14dcfcd923aa38a98f709d931fc61c367e21803
xiang95 <xiang95@node1.flanneliptable.meshbench-pg0.apt.emulab.net>
2026-02-03 21:52:40 -0700

    Add more sample cases

---
+++ kafel/sample_and_eq_short_top/sample_and_eq_short_top_ori.rkt
@@ -0,0 +1,57 @@
+#lang rosette/safe
+
+(require rosette/lib/synthax)
+(require rosette/base/base)
+
+(define int32? (bitvector 32))
+
+(define-symbolic buflo (bitvector 32))
+(define-symbolic bufhi (bitvector 32))
+
+(define-symbolic cost0 cost0L cost0R integer?)
+(assert (and (>= cost0 0)))
+
+(define (spec buflo bufhi)
+  (cond
+    [(bveq (bvand buflo (bv #xf1f1f1f1 32)) (bv #xf1f1f1f1 32)) (bv 0 1)] ;; ALLOW
+    [else               (bv 1 1)]))   ;; KILL
+
+(define (impl buflo bufhi)
+  (let* (
+         [cond-expr0 ((choose bveq bvult bvugt bvule bvuge (not bveq)) 
+         (choose buflo ((choose bvand bvor) (choose buflo bufhi (?? int32?)) (choose buflo bufhi (?? int32?))) )
+         (choose (?? int32?) buflo bufhi))]
+         
+         [expr0L (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          )
+         ]
+         [expr0R (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          )
+         ]
+
+         [choiceexpr0 (choose 0 1 2)]
+         [expr0 (cond
+            [(= choiceexpr0 0) (bv 0 1)]  ;;; ALLOW
+            [(= choiceexpr0 1) (bv 1 1)]  ;;; KILL
+            [else (if cond-expr0 expr0L expr0R)]
+            )
+         ]
+         [_ (if (or (= choiceexpr0 0) (= choiceexpr0 1)) (assert (= cost0 0)) (assert (= cost0 1)))]
+         )
+    expr0)
+)
+
+(define total-cost cost0)
+
+;; --- optimize to minimize total cost ---
+(define sol
+   (synthesize
+     #:forall    (list buflo bufhi)
+     #:guarantee (begin
+      (assert (and (bveq (impl buflo bufhi) (spec buflo bufhi))))
+    ))
+)
+
+(print-forms sol)
---
+++ kafel/sample_or_flags/sample_or_flags_op.rkt
@@ -0,0 +1,89 @@
+#lang rosette/safe
+
+(require rosette/lib/synthax)
+(require rosette/base/base)
+
+(define-symbolic flag (bitvector 32))
+
+(define-symbolic cost0 cost0L cost0R integer?)
+(assert (and (>= cost0 0) (>= cost0L 0) (>= cost0R 0)))
+
+(define (spec flag)
+  (cond
+    [(or (bveq flag (bv 66 32)) (bveq flag (bv 65 32))) (bv 0 1)] ;; ALLOW
+    [else               (bv 1 1)]))   ;; KILL
+
+(define (impl flag)
+  (let* (
+         [cond-expr0 ((choose bveq bvult bvugt bvule bvuge (not bveq)) 
+         (choose flag ((choose bvand bvor) (choose flag (bv 66 32) (bv 65 32)) (choose flag (bv 66 32) (bv 65 32))) )
+         (choose (bv 3261507367 32) (bv 66 32) (bv 65 32) flag))]
+
+         [cond-expr0L ((choose bveq bvult bvugt bvule bvuge (not bveq)) 
+         (choose flag ((choose bvand bvor) (choose flag (bv 66 32) (bv 65 32)) (choose flag (bv 66 32) (bv 65 32))) )
+         (choose (bv 66 32) (bv 65 32) flag))]
+
+         [cond-expr0R ((choose bveq bvult bvugt bvule bvuge (not bveq)) 
+         (choose flag ((choose bvand bvor) (choose flag (bv 66 32) (bv 65 32)) (choose flag (bv 66 32) (bv 65 32))) )
+         (choose (bv 66 32) (bv 65 32) flag))]
+
+         
+         [expr0LL (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          )
+         ]
+         [expr0LR (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          )
+         ]
+         [expr0RL (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          )
+         ]
+         [expr0RR (choose (bv 0 1)  ;;; ALLOW
+                          (bv 1 1)  ;;; KILL
+                          )
+         ]
+
+         [choiceexpr0L (choose 0 1 2)]
+          [expr0L (cond
+              [(= choiceexpr0L 0) (bv 0 1)]  ;;; ALLOW
+              [(= choiceexpr0L 1) (bv 1 1)]  ;;; KILL
+              [else (if cond-expr0L expr0LL expr0LR)]
+              )
+          ]
+        [_ (if (or (= choiceexpr0L 0) (= choiceexpr0L 1)) (assert (= cost0L 0)) (assert (= cost0L 1)))]
+
+         [choiceexpr0R (choose 0 1 2)]
+         [expr0R (cond
+            [(= choiceexpr0R 0) (bv 0 1)]  ;;; ALLOW
+            [(= choiceexpr0R 1) (bv 1 1)]  ;;; KILL
+            [else (if cond-expr0R expr0RL expr0RR)]
+            )
+         ] 
+         [_ (if (or (= choiceexpr0R 0) (= choiceexpr0R 1)) (assert (= cost0R 0)) (assert (= cost0R 1)))]
+
+         [choiceexpr0 (choose 0 1 2)]
+         [expr0 (cond
+            [(= choiceexpr0 0) (bv 0 1)]  ;;; ALLOW
+            [(= choiceexpr0 1) (bv 1 1)]  ;;; KILL
+            [else (if cond-expr0 expr0L expr0R)]
+            )
+         ]
+         [_ (if (or (= choiceexpr0 0) (= choiceexpr0 1)) (assert (= cost0 0)) (assert (= cost0 1)))]
+         )
+    expr0)
+)
+
+(define total-cost (+ cost0 cost0L cost0R))
+
+;; --- optimize to minimize total cost ---
+(define sol
+   (synthesize
+     #:forall    (list flag)
+     #:guarantee (begin
+      (assert (and (bveq (impl flag) (spec flag)) (= total-cost 2)))
+    ))
+)
+
+(print-forms sol)
